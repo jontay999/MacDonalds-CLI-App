c01e005d88d8785a06b44119cffebfbcb420e2df
Jonathan Tay <jonathan.tay@searix.net>
2021-10-20 02:14:10 +0800

    First commit

---
+++ MacDonaldsApp.java
@@ -0,0 +1,11 @@
+package MacDonalds;
+
+
+public class MacDonaldsApp {
+
+    public static void main(String[] args){
+        System.out.println("Hello! Welcome to MacDonalds");
+
+        }
+    }
+
