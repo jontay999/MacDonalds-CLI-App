c014bce197afaedab1ebd6c96d706cfbd6cac0e7
Jonathan Tay <jonathan.tay@searix.net>
2021-10-20 22:49:06 +0800

    Revert "added a bit of data for menuitems"
    This reverts commit fb21254db50486eb7a029e04f1d98657e8bc92de.

--- Restaurant.java
+++ Restaurant.java
@@ -28,6 +28,10 @@ public class Restaurant {
         this.location = location;
     }
 
+    //for month
+    private void generateRevenueReport(int year, int month){
+
+    }
 
     //for daily
     private void generateRevenueReport(LocalDate selectedDate){
@@ -71,6 +75,7 @@ public class Restaurant {
         printRevenueReport(alaCarteSales, promoSales);
     }
 
+
     private void printRevenueReport(Map<String, HashMap<String, Double>> alaCarteSales, Map<String, HashMap<String, Double>> promoSales){
         double alaCarteRevenue = 0;
         double promoRevenue = 0;
@@ -85,6 +90,8 @@ public class Restaurant {
         System.out.println("===============================");
         System.out.printf("Total Revenue : %.2f%n", alaCarteRevenue+promoRevenue);
     }
+
+
 
     private double printCategorySales(Map<String, HashMap<String, Double>> salesCategory){
         double totalSales = 0;
@@ -141,8 +148,6 @@ public class Restaurant {
     public boolean isOpen(LocalTime checkingTime){
         return checkingTime.isBefore(getClosingTime()) && checkingTime.isAfter(getOpeningTime());
     }
-
-
 
     public LocalTime getOpeningTime() {
         return openingTime;
