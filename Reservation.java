6df30e3dc50428c724b7cfaf7836c841c86ed8d5
marctan2705 <67046774+marctan2705@users.noreply.github.com>
2021-10-25 09:47:19 +0800

    hello world

--- Reservation.java
+++ Reservation.java
@@ -2,5 +2,80 @@ package MacDonalds;
 import java.time.LocalDateTime;
 
 public class Reservation {
-    public LocalDateTime reservationTime;
+    private LocalDateTime reservationTiming;
+    private int reservationID;
+    private LocalDateTime createdAt;
+    private int numPax;
+    private String customerName;
+    private String restaurantName;
+    private String status;
+    Reservation(){
+    }
+    Reservation(LocalDateTime reservationTiming, int reservationID, LocalDateTime createdAt, int numPax, String customerName, String restaurantName, String status){
+        this.reservationTiming = reservationTiming;
+        this.reservationID = reservationID;
+        this.createdAt = createdAt;
+        this.numPax = numPax;
+        this.customerName = customerName;
+        this.restaurantName = restaurantName;
+        this.status = status;
+    }
+
+
+
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public int getNumPax() {
+        return numPax;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getReservationTime() {
+        return reservationTime;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setNumPax(int numPax) {
+        this.numPax = numPax;
+    }
+
+    public void setReservationTime(LocalDateTime reservationTime) {
+        this.reservationTime = reservationTime;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- Table.java
+++ Table.java
@@ -1,4 +1,110 @@
 package MacDonalds;
 
 public class Table {
+    private int tableNum;
+    private int size;
+    private boolean isOccupied;
+    private boolean isReserved;
+    private MacDonalds.Reservation currentReservation;
+    private MacDonalds.Reservation[] fulfilledReservations;
+    private MacDonalds.Reservation[] pendingReservations;
+    private LocalDateTime[] reservedTimings;
+    private LocalDateTime nextReservationTime;
+    Table(){
+        size=0;
+        tableNum=0;
+    }
+    Table(int tableNum, int size){
+        this.tableNum = tableNum;
+        this.size = size;
+    }
+
+    public int getReservationID() {
+        return this.currentReservation.getReservationID();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getTableNum() {
+        return tableNum;
+    }
+
+    public LocalDateTime getNextReservationTime() {
+        return nextReservationTime;
+    }
+
+    public boolean getIsOccupied() {
+        return isOccupied;
+    }
+
+    public boolean getIsReserved() {
+        return isReserved;
+    }
+
+    public LocalDateTime[] getReservedTimings() {
+        return reservedTimings;
+    }
+
+    public MacDonalds.Reservation getCurrentReservation() {
+        return currentReservation;
+    }
+
+    public MacDonalds.Reservation[] getFulfilledReservations() {
+        return fulfilledReservations;
+    }
+
+    public MacDonalds.Reservation[] getPendingReservations() {
+        return pendingReservations;
+    }
+
+    public void setCurrentReservation(MacDonalds.Reservation currentReservation) {
+        this.currentReservation = currentReservation;
+    }
+
+    public void setFulfilledReservations(MacDonalds.Reservation[] fulfilledReservations) {
+        this.fulfilledReservations = fulfilledReservations;
+    }
+
+    public void setPendingReservations(MacDonalds.Reservation[] pendingReservations) {
+        this.pendingReservations = pendingReservations;
+    }
+
+    public void setReservedTimings(LocalDateTime[] reservedTimings) {
+        this.reservedTimings = reservedTimings;
+    }
+
+    public void setNextReservationTime(LocalDateTime nextReservationTime) {
+        this.nextReservationTime = nextReservationTime;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.currentReservation.setReservationID(reservationID);
+    }
+
+    public void setReserved(boolean reserved) {
+        isReserved = reserved;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setTableNum(int tableNum) {
+        this.tableNum = tableNum;
+    }
+    public boolean isAvailable(LocalDateTime time){
+        boolean availability = true;
+        for(int i = 0; i<this.getReservedTimings().length; i++){
+           if(this.getReservedTimings()[i] == time){
+               availability = false;
+           }
+       }
+       return availability;
+    }
 }
